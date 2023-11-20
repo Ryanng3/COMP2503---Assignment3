1fd0a4a82102b95b52b88ab6690b758d2a4e13b0
rnguy774 <rnguy774@mtroyal.ca>
2023-11-20 01:06:15 -0700

    Implemented BST

---
+++ Assignment3/src/mru/application/BST.java
@@ -0,0 +1,177 @@
+package mru.application;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BST<T extends Comparable<T>> {
+	class BSTNode implements Comparable<BSTNode> {
+		private T data;
+		private BSTNode left;
+		private BSTNode right;
+
+		public BSTNode(T d) {
+			setLeft(null);
+			setRight(null);
+			setData(d);
+		}
+		
+		public T getData() {
+			return data;
+			}
+		
+		public void setData(T d) {
+			data = d;
+			}
+		public void setLeft(BSTNode l) {
+			left = l;
+			}
+		public void setRight(BSTNode r) {
+			right = r;
+		}
+		
+		public BSTNode getLeft() {
+			return left;
+			}
+		
+		public BSTNode getRight() {
+			return right;
+			}
+		
+		public boolean isLeaf() {
+			return (getLeft() == null) && (getRight() == null);
+			}
+		
+		public int compareTo(BSTNode o) {
+			return this.getData().compareTo(o.getData());
+		}
+	}
+
+	private BSTNode root;
+	private int size;
+
+	public BST() {
+		root = null;
+		size = 0;
+	}
+	
+	public int size() {
+		return size;
+	}
+
+	public void add(T d) {
+		BSTNode n = new BSTNode(d);
+		if (root == null) {
+			root = n;
+		} else {
+			add(root, n);
+		}
+		size++;
+	}
+
+	public int height() {
+		return height(root);
+	}
+
+	public void printInOrder() {
+		inOrderTraversal(root);
+	}
+	
+	public void printPreOrder() {
+		preOrderTraversal(root);
+	}
+	
+	public void printPostOrder() {
+		postOrderTraversal(root);
+	}
+	
+	public void printLevelOrder() {
+		levelOrderTraversal(root);
+	}
+
+	private void add(BSTNode r, BSTNode n) {
+		int c = n.compareTo(r);
+		if(c < 0) {
+			if(r.getLeft() == null) {
+				r.setLeft(n);
+			}else {
+				add(r.getLeft(), n);
+			}
+		}else if(c > 0) {
+			if(r.getRight() == null) {
+				r.setRight(n);
+			}else {
+				add(r.getRight(), n);
+			}
+		}
+	}
+
+	private int height(BSTNode r) {
+		int h = -1;
+	    if (r == null) {
+	        return h;
+	    } else if (r.getLeft() == null && r.getRight() == null) {
+	        return 0;
+	    } else {
+	        return 1 + Math.max(height(r.getLeft()), height(r.getRight()));
+	    }
+	}
+
+	private void visit(BSTNode r) {
+		if (r != null)
+			System.out.println(r.getData());
+	}
+	
+	private void inOrderTraversal(BSTNode r) {
+		if (r == null)
+			return;
+		else {
+			inOrderTraversal(r.getLeft());
+			visit(r);
+			inOrderTraversal(r.getRight());
+		}
+	}
+	
+	private void preOrderTraversal(BSTNode r) {
+		if(r == null) {
+			return;
+		}else {
+			visit(r);
+			preOrderTraversal(r.getLeft());
+			preOrderTraversal(r.getRight());
+			
+		}
+	}
+	
+	private void postOrderTraversal(BSTNode r) {
+		if(r == null) {
+			return;
+		}else {
+			postOrderTraversal(r.getLeft());
+			postOrderTraversal(r.getRight());
+			visit(r);
+		}
+	}
+	
+	private void levelOrderTraversal(BSTNode r) {
+		  if (r == null) {
+		        return;
+		    }
+
+		    Queue<BSTNode> queue = new LinkedList<>();
+		    queue.add(root);
+
+		    while (!queue.isEmpty()) {
+		        BSTNode current = queue.poll();
+		        visit(current);
+
+		        if (current.getLeft() != null) {
+		            queue.add(current.getLeft());
+		        }
+
+		        if (current.getRight() != null) {
+		            queue.add(current.getRight());
+		        }
+		    }
+		
+	}
+}
